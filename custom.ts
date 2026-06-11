55c5ffaa648f8658230eef7650e18b27a0187120
V-code-japan <minecraft@e-sia.jp>
2026-06-11 18:15:46 +0900

    更新 main.blocks, custom.ts

--- custom.ts
+++ custom.ts
@@ -4,39 +4,20 @@
 * 詳しくはこちらを参照してください：https://minecraft.makecode.com/blocks/custom
 */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
-
-/**
- * Custom blocks
- */
 //% weight=100 color=#0fbc11 icon=""
 namespace ATM {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
 
     //% block="プレイヤーがお金をもっている"
-    let hasMoney: boolean;
+    export let hasMoney: boolean;
     
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
+    //% block="お金を預ける 金額: $money"
+    export function depositMoney(money: Number): void {
         // Add code here
     }
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    //% block="お金を引き出す 金額: $money"
+    export function withdrowMoney(money: Number): void {
+
     }
+
 }
